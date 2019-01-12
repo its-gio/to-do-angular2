a9cdc03b49ca8b8d2533c85907e89c51c070b54d
its-gio <me@its-g.io>
2019-01-12 02:05:01 -0800

    Adding and exporting ui-login component and creating working router in main app

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -4,23 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
 import { CustomersModule } from './customers/customers.module';
 import { ProjectModule } from './project/project.module';
+import { UiLoginModule } from '@workshop/ui-login';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     HomeModule,
     CustomersModule,
-    ProjectModule
+    ProjectModule,
+    UiLoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
