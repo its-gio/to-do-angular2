94a7fc0d166fe4ff6aa9b5d16608b3219f9ed1ca
its-gio <me@its-g.io>
2019-01-11 22:04:02 -0800

    Generating customers and project, component and module then adding all created modules to app module to access MaterialModule

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProjectModule } from './project/project.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +17,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HomeModule,
+    CustomersModule,
+    ProjectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
