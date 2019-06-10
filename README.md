4267d3ae3874b880323f293c0d8f22e4f69c6fce
its-gio <me@its-g.io>
2019-06-09 18:16:38 -0700

    Added read.me with light details on connecting modules and components. Also created and connected todos component to todo-item component. Light styles to todo-item component.

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+### Notes
+
+ng generate component _folder/componentName_
+Add component to main app.module.ts file for use
+Use comonent selector as html tag in main app.component.html
+Create models folder and create a class of variables then connect it to components _component.ts_
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../../models/Todo";
+
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.scss"]
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: "todo one",
+        complete: false
+      },
+      {
+        id: 2,
+        title: "todo two",
+        complete: false
+      },
+      {
+        id: 3,
+        title: "todo two",
+        complete: false
+      }
+    ];
+  }
+}
