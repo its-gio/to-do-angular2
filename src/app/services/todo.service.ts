4f292d0f013d0f9ef8dc0c81bc27d89f125b54d1
its-gio <me@its-g.io>
2019-06-25 20:51:39 -0700

    Adding limit to jsonplaceholder api

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 import { Todo } from "../models/Todo";
-import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class TodoService {
-  todosURL = "https://jsonplaceholder.typicode.com/todos";
+  todosURL: string = "https://jsonplaceholder.typicode.com/todos";
+  todosLimit: string = "?_limit=10";
 
   // Creating private variable http and giving it the tupe of HttpClient
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<Todo[]> {
     // Using http get request on todosURL with the type of Todo (modules/Todo)
-    return this.http.get<Todo[]>(this.todosURL);
+    return this.http.get<Todo[]>(`${this.todosURL}${this.todosLimit}`);
   }
 }
