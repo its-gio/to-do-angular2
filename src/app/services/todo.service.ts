33a1ab054d0149ea30470ef14f4a534efe894365
its-gio <me@its-g.io>
2019-06-25 21:03:39 -0700

    Added toggle to API Server

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,12 @@ import { Observable } from "rxjs";
 
 import { Todo } from "../models/Todo";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
+
 @Injectable({
   providedIn: "root"
 })
@@ -17,5 +23,10 @@ export class TodoService {
   getTodos(): Observable<Todo[]> {
     // Using http get request on todosURL with the type of Todo (modules/Todo)
     return this.http.get<Todo[]>(`${this.todosURL}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosURL}/${todo.id} `;
+    return this.http.put(url, todo, httpOptions);
   }
 }
